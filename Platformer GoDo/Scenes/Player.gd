8777fb9eeeca4ab46692bc4cff752020b97eba3a
TerriWil <90724133+TerriWil@users.noreply.github.com>
2021-10-05 16:40:18 -0400

    10052021

--- Platformer GoDo/Scenes/Player.gd
+++ Platformer GoDo/Scenes/Player.gd
@@ -1,33 +1,34 @@
 extends KinematicBody2D
 
-const GRAVITY = 35
-
-var velocity = Vector2(0,0)
-
-export var speed= 100
-export var jump = -900
+var velocity := Vector2(0,0)
+export var speed := 180
+export var jump := -900
+const GRAVITY := 35
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("right"):
+	if Input.is_action_pressed("right"):
+		velocity.x = speed
 		$AnimatedSprite.play("Walk")
 		$AnimatedSprite.flip_h = false
-		velocity.x= speed
 	elif Input.is_action_pressed("left"):
+		velocity.x = -speed
 		$AnimatedSprite.play("Walk")
 		$AnimatedSprite.flip_h = true
-		velocity.x=-speed
 	else:
-		velocity.x=0
-	
-	
+		$AnimatedSprite.play("Idle")
+	#show with "pressed"
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y += jump
 		
 	if not is_on_floor():
-		$AnimatedSprite.play("jump")
-		#get_node ("AnimatedSprite")
-		velocity.y += GRAVITY
-		print(velocity)
-		
-	move_and_slide(velocity,Vector2.UP)
-	
+		$AnimatedSprite.play("Jump")
+
+	velocity.y += GRAVITY
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+	velocity.x = lerp(velocity.x, 0,0.1)
+
+func _on_Area2D_body_entered(body):
+		get_tree(). change_scene("res://Scenes/Level 1.tscn")
+		pass # Replace with function body.
