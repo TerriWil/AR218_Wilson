e6c727815bf0176dbde580fe9c174a834b37539a
TerriWil <90724133+TerriWil@users.noreply.github.com>
2021-10-12 17:14:37 -0400

    10122021-New Update

--- Platformer GoDo/Scenes/Sanitizer.gd
+++ Platformer GoDo/Scenes/Sanitizer.gd
@@ -1,8 +1,18 @@
 extends Area2D
 
 
-
+signal money_collected
 func _on_Sanitizer_body_entered(body):
+	emit_signal("money_collected")
+	
 	queue_free()
-	pass # Replace with function body.
-	GDScript.is_instance_valid()
+	
+	Global.score = Global.score + 1
+	print(Global.score)
+	
+	
+	
+
+	set_collision_layer_bit(3,false)
+	set_collision_mask_bit(0,false)
+	
---
+++ Platformer GoDo/Scenes/ScoreLabel.gd
@@ -0,0 +1,12 @@
+extends Label
+
+
+
+var my_string = "Cleaning power collected"
+
+func _on_Sanitizer_money_collected():
+	pass # Replace with function body.
+	
+	
+	
+	text = String(Global.score) + my_string
