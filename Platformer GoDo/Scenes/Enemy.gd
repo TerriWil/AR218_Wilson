e9a679b375a60771c00c570cd414c8dfc81054e0
TerriWil <90724133+TerriWil@users.noreply.github.com>
2021-10-19 17:45:44 -0400

    10192021-Update

--- Platformer GoDo/Scenes/Enemy.gd
+++ Platformer GoDo/Scenes/Enemy.gd
@@ -3,14 +3,15 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 
+export var direction = 1
+
+export var can_fall = false
 
 export var speed = 50
 
 
 const GRAVITY := 35
 
-
-export var direction = 1
 
 func _ready():
 	if direction == -1:
@@ -20,22 +21,56 @@ func _ready():
 		$AnimatedSprite. flip_h()
 		
 func  _process(delta):
-		if is_on_wall():
+	if not $Fade_Timer.is_stopped():
 		
+		
+		modulate.a =modulate.a * $Fade_Timer.time_left
+		
+		
+		
+	if is_on_wall():
+		flip_enemy()
+	if not can_fall:
+		if not $ground_checker .is_colliding and is_on_floor():
 			flip_enemy()
-		velocity.y += GRAVITY
-		velocity.X += speed * direction
-		velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.y += GRAVITY
+	velocity.x = speed * direction
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
 func flip_enemy():
-		direction = direction * -1
-		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
+	direction = direction * -1
+	$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
+	$ground_check.position.x = $ground_check.position.x *-1 
+	if can_fall:
+		$ground_check.enable = false
+
+	velocity.y += GRAVITY
+
+
+	velocity.x += speed * direction 
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func _on_player_checker_body_entered(body):
+	
+	if body .name == "Player":
 		
-		$ground_check.position.x = $ground_check.position.x *-1 
+		body.energy = body.energy - 1
+		print(body.energy)
+		if body.energy  <= 0:
+			get_tree ().cahnge_scene("res://Scenes/Level 1.tscn")
+		$Fade_Timer.start()
+		
+		$AnimatedSprite.playing = false
+		set_collision_layer_bit(5,false)
+		set_collision_mask_bit(0,false)
+		$player_checker. set_collision_layer_bit(5,false)
+		$player_checker. set_collision_mask_bit(0,false)
+		
+		speed = 0 
+			
 
-		velocity.y += GRAVITY
-
-
-		velocity.x += speed * direction 
-
-		velocity = move_and_slide(velocity, Vector2.UP)
+func _on_Fade_Timer_timeout():
+	
+	queue_free()
