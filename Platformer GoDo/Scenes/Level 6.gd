9e2416126ac3866338a0538022892640839a57a4
TerriWil <90724133+TerriWil@users.noreply.github.com>
2021-10-28 18:50:14 -0400

    10282021-Update

---
+++ Platformer GoDo/Scenes/Level 6.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+func _on_Goal_body_entered(body):
+	var e = $Enemies.get_child_count()
+	var i = $Money.get_child_count()
+	if e == 0 and i == 0 :
+		print ("change scene")
+		get_tree().change_scene("res://Scenes/Level7.tscn")
+	else:
+		print(i)
+		print(e)
+	pass # Replace with function body.
--- Platformer GoDo/Scenes/Menu.gd
+++ Platformer GoDo/Scenes/Menu.gd
@@ -4,3 +4,18 @@ extends Node2D
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Level 1.tscn")
+
+
+func _on_Statement_pressed():
+	get_tree().change_scene("res://Scenes/Artist Statement.tscn")
+	pass # Replace with function body.
+
+
+func _on_Instructions_pressed():
+	get_tree().change_scene("res://Scenes/Instruction.tscn")
+	pass # Replace with function body.
+
+
+func _on_Main_Menu_pressed():
+	get_tree().change_scene("res://Scenes/Menu.tscn")
+	pass # Replace with function body.
